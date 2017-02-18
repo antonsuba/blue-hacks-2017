bd241d1cb914f83b2b6fe89af27f873902a791d8
Fiel Faustino <thaf.faustino@gmail.com>
2017-02-18 22:33:55 +0800

    Fix Models

--- app/Conversation.php
+++ app/Conversation.php
@@ -10,4 +10,8 @@ class Conversation extends Model
 	public function users(){
         return $this->belongsToMany('App\User');
     }
+
+    public function messages(){
+        return $this->hasMany('App\Message');
+    }
 }
