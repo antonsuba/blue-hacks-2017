e0311fcbb7b01a30afb75fbd0f93385ddc71d6f0
Eric Sun <Eric Sun>
2017-02-19 01:42:22 +0800

    Create_Users_table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-			$table->string('facebook_id');
+			$table->bigInteger('facebook_id');
 			$table->string('avatar');
             $table->string('name');
             $table->string('email')->unique();
