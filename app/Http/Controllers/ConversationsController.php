aaa0e8fbb3851ab5978fffa5773bb5c492ee72a0
Fiel Faustino <thaf.faustino@gmail.com>
2017-02-18 21:30:48 +0800

    Create Conversations Controller

---
+++ app/Http/Controllers/ConversationsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ConversationsController extends Controller
+{
+    public function retrieveMessages(Request $request){
+        $inputs = $request->input();
+        $currentUser = Auth::user();
+        $conversation;
+
+        $checkAdviser = User_Types::where('user_id', Auth::id(), 'category_id', $inputs['categoryID'])->get()   ;
+        if($checkAdviser->isEmpty()){
+            //user is advisee
+            $conversation = Conversation::where('advisee_id', $inputs['userID'], 'adviser_id', Auth::id());
+        } else{
+            //user is advisor
+            $conversation = Conversation::where('advisee_id', Auth::id(), 'adviser_id', $inputs['userID']);
+        }
+
+        $messages = Message::where('user_id', Auth::id(), 'conversation_id', $conversation->id);
+
+        return $messages;
+    }
+
+    public function sendMessage(Request $request){
+        $inputs = $request->input();
+
+        $message = new Message;
+        $message->content = $inputs['content'];
+        $message->user_id = Auth::id();
+        $message->save();
+    }
+}
